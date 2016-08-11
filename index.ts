f006842d8924be1585320c08a66c7bc7209c7f13
junjun16818 <junjun16818@hotmail.com>
2016-08-12 00:10:02 +0800

    use typescript2 build and test

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 import _api = require('./lib/request');
 
 namespace K8s {
-    export type K8sRequest = _api.Request;
     export var api = require('./lib/request');
     export var kubectl = require('./lib/kubectl');
 }
 
-export default K8s;
+export = K8s;
