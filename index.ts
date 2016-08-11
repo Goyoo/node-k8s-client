f78beb22b49a67859475fabd65742fd94e6420bf
junjun16818 <junjun16818@hotmail.com>
2016-08-12 02:13:40 +0800

    fix

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
-import { Request } from './lib/request';
+import { Request } from './lib/request'
 
 namespace K8s {
-    export type K8sRequest = Request;
+    export type K8sRequest = Request
     export var api = (conf) => {
-        return new Request(conf);
-    };
-    export var kubectl = require('./lib/kubectl');
+        return new Request(conf)
+    }
+    export var kubectl = require('./lib/kubectl')
 }
 
-export = K8s;
+export = K8s
